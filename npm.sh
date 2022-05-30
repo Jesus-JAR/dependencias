899767f3af22f0dbe674865cfd0b8eca3830ec8d
Jesus Arraras <jesusarraras1@gmail.com>
2022-05-30 17:00:04 +0200

    npm

--- npm.sh
+++ npm.sh
@@ -8,6 +8,13 @@ sudo apt update && sudo apt upgrade -y
 
 echo Instalacion
 sleep 10s
+
 sudo apt install nodejs npm
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
 
 echo exit.
