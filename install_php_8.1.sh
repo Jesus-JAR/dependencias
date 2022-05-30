46ad2dda54c614911514ef9613c5bb5c0d9a29bc
Jesus Arraras <jesusarraras1@gmail.com>
2022-05-30 16:23:30 +0200

    php_8.1

---
+++ install_php_8.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo Instalar php 8.1
+echo actualizar sistema
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install ca-certificates apt-transport-https software-properties-common wget curl lsb-release -y
+curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
+sudo apt update
+sudo apt upgrade
+sudo apt install php8.1 libapache2-mod-php8.1 -y
+sudo systemctl restart apache2
+
+echo Instalado php 8.1
+echo exit.
